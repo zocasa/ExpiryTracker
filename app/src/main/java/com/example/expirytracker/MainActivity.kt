a1ac10933cf4cffcd9fe5da73c505e045229f009
Arpit Krishna <krishnaarpit95@gmail.com>
2024-01-29 18:44:45 -0800

    Add Edit and Delete buttons (non-functional)

--- app/src/main/java/com/example/expirytracker/MainActivity.kt
+++ app/src/main/java/com/example/expirytracker/MainActivity.kt
@@ -28,6 +28,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ElevatedButton
@@ -87,8 +89,32 @@ class MainActivity : ComponentActivity() {
     uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Composable
-fun ItemInfoCardPreview() {
-    App()
+fun CollapsedComponentPreview() {
+    val itemInfo = ItemInfo(
+        "Milk",
+        sortedSetOf(ExpiryDate(30, 1, 2024), ExpiryDate(15, 2, 2024)),
+        "https://www.amazon.com/mery-Grass-Organic-Whole-Supportibackl-248/dp/B06ZYNZ7NF"
+    )
+    ItemInfoCard(itemInfo, false)
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true
+)
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun ExpendedComponentPreview() {
+    val itemInfo = ItemInfo(
+        "Milk",
+        sortedSetOf(ExpiryDate(30, 1, 2024), ExpiryDate(15, 2, 2024)),
+        "https://www.amazon.com/mery-Grass-Organic-Whole-Supportibackl-248/dp/B06ZYNZ7NF"
+    )
+    ItemInfoCard(itemInfo, true)
 }
 
 @Composable
@@ -210,10 +236,10 @@ fun CategoryText(text: String) {
 }
 
 @Composable
-fun ItemInfoCard(itemInfo: ItemInfo) {
+fun ItemInfoCard(itemInfo: ItemInfo, shouldExpend: Boolean = false) {
     val itemColor = getItemColor(itemInfo.daysRemaining, WARNING_DAYS, SAFE_DAYS)
 
-    var isExpended by remember { mutableStateOf(false) }
+    var isExpended by remember { mutableStateOf(shouldExpend) }
     val surfaceColor by animateColorAsState(
         if (isExpended) MaterialTheme.colorScheme.surface else getColorWithAlpha(itemColor, 0.25F),
         label = "ItemInfo Card surface color"
@@ -229,19 +255,40 @@ fun ItemInfoCard(itemInfo: ItemInfo) {
             .clickable { isExpended = !isExpended }
             .animateContentSize()
     ) {
-        Row {
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 8.dp, vertical = 4.dp)
-                    .weight(1F)
-            ) {
-                ItemTitleAndExpiryIndicator(itemInfo, itemColor, isExpended)
-                Spacer(modifier = Modifier.height(1.dp))
-                ItemDetailAndAdditionalInformation(itemInfo, isExpended)
+        Column {
+            Row {
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                        .weight(1F)
+                ) {
+                    ItemTitleAndExpiryIndicator(itemInfo, itemColor, isExpended)
+                    Spacer(modifier = Modifier.height(1.dp))
+                    ItemDetailAndAdditionalInformation(itemInfo, isExpended)
+                }
+
+                if (!isExpended)
+                    ItemProductLink(itemInfo, Modifier.padding(all = 8.dp))
             }
 
-            if (!isExpended)
-                ItemProductLink(itemInfo, Modifier.padding(all = 8.dp))
+            if (isExpended) {
+                IconButton(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .size(25.dp),
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        Icons.Filled.Delete,
+                        "Delete Item",
+                        Modifier.size(20.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(2.dp))
+            }
         }
     }
 }
@@ -288,7 +335,8 @@ fun ItemDetailAndAdditionalInformation(itemInfo: ItemInfo, isExpended: Boolean) 
         )
     } else {
         Column(
-            verticalArrangement = Arrangement.spacedBy(4.dp)
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier.padding(top = 3.dp)
         ) {
             itemInfo.expiryDates.forEach {
                 val daysRemaining = ChronoUnit.DAYS.between(CURRENT_TIME, it.localDate)
@@ -302,12 +350,52 @@ fun ItemDetailAndAdditionalInformation(itemInfo: ItemInfo, isExpended: Boolean) 
                         .border(0.25.dp, getColorWithAlpha(itemColor, 0.5F), MaterialTheme.shapes.small)
                         .border(0.125.dp, Color.DarkGray, MaterialTheme.shapes.small)
                 ) {
-                    Text(
-                        style = MaterialTheme.typography.bodySmall,
-                        color = MaterialTheme.colorScheme.primary,
-                        text = "${it.date} ${it.displayableMonth}, ${it.year}",
-                        modifier = Modifier.padding(all = 4.dp)
-                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Spacer(modifier = Modifier.width(2.dp))
+
+                        Text(
+                            style = MaterialTheme.typography.bodySmall,
+                            color = MaterialTheme.colorScheme.primary,
+                            text = "${it.date} ${it.displayableMonth}, ${it.year}",
+                            modifier = Modifier
+                                .padding(all = 4.dp)
+                                .weight(1F)
+                        )
+
+                        IconButton(
+                            modifier = Modifier
+                                .size(20.dp),
+                            onClick = {
+
+                            }
+                        ) {
+                            Icon(
+                                Icons.Filled.Edit,
+                                "Edit Entry",
+                                Modifier.size(15.dp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(2.dp))
+
+                        IconButton(
+                            modifier = Modifier
+                                .size(20.dp),
+                            onClick = {
+
+                            }
+                        ) {
+                            Icon(
+                                Icons.Filled.Delete,
+                                "Delete Entry",
+                                Modifier.size(15.dp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(4.dp))
+                    }
                 }
             }
         }
