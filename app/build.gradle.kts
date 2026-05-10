6488d28d69f04a1d86e31266492e3754582a26df
Arpit Krishna <krishnaarpit95@gmail.com>
2026-05-10 18:33:14 +0530

    AGP 9.2.1 upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
